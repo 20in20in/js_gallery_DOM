3d38fe69f991488b636be72069b607582c31c3d8
Igor Novikov <novikov.igor.20@gmail.com>
2024-07-22 21:08:31 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const galleryList = document.querySelector(".gallery__list");
+const galleryLargeImg = document.querySelector(".gallery__large-img");
+galleryList.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const link = e.target.closest(".list-item__link");
+    if (link) galleryLargeImg.src = link.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
